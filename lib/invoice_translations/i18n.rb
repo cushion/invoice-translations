3db5789e1e9318da7d2e5a9068fbcbee1f80f254
Larry Fox <larrywfox@gmail.com>
2016-08-26 10:48:02 -0400

    Remove r18n dependency

--- lib/invoice_translations/i18n.rb
+++ lib/invoice_translations/i18n.rb
@@ -1,17 +1,17 @@
-require 'r18n-core'
+require 'yaml'
 
 module InvoiceTranslations
   class I18n
     def initialize(locale)
-      @i18n = R18n::I18n.new(locale, LOADER)
+      @i18n = ::YAML.load_file(File.join(I18N_DIR, "#{locale}.yml"))
     end
 
     def to_h
-      @i18n.t.to_hash.select { |k| %w[description invoice].freeze.include?(k) }
+      @i18n
     end
 
     def [](name)
-      @i18n[:invoice][name]
+      @i18n['invoice'.freeze][name.to_s]
     end
     alias method_missing []
   end
